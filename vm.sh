10942a06a3aca323e76d7a0142fbb9301497699c
Vinay Pursnani <52756604+vinaypursnani@users.noreply.github.com>
2021-09-12 00:32:38 +0530

    Create vm.sh
    On a newly deployed virtual machine, we install necessary packages through the shell and clone the existing GoogleCloud repository which includes <earthquakes.csv> data to generate a png from that data through <transform.py> we install necessary dependencies and those are pre-mentioned in an existing shell file named 
    <install_missing.sh> Before going ahead and running <transform.py> we run <ingest.sh> to delete old data and get the latest earthquakes data through the official URL.

---
+++ vm.sh
@@ -0,0 +1,18 @@
+$ sudo apt-get install git
+$ git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+$ cd training-data-analyt
+$ cd courses
+$ cd bdml_fundamentals
+$ cd earthquakesvm
+
+$ less ingest.sh
+$ less transform.py
+$ cat install_missing.sh
+$ ./install_missing.sh
+$ ./ingest.sh
+$ sudo apt-get install wget
+$ head earthquakes.csv
+$ ./transform.py
+$ ls *.png
+$ gsutil ls gs://bucket_name
+$ gsutil cp earthquakes.* gs://bucket_name
